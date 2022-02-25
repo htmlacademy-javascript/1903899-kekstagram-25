f81735f0b7766a0507a882e38e33ee1802b8ba26
Jeenlevel <inlevel94@gmail.com>
2022-02-25 17:07:07 +0300

    исправления 2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 //источник https://learn.javascript.ru/task/random-int-min-max
 function randomInteger(min, max) {
-  let rand = min + Math.random() * (max + 1 - min);
+  const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
-};
+}
 randomInteger(1,3);
 
-function stringLength(сheckinString,maxLength) {
+function checkStringLength(сheckinString,maxLength) {
   if (length <= maxLength) {
-  return false;
+    return false;
   }
   return true;
 }
-stringLength(1,9);
+checkStringLength(2,9);
