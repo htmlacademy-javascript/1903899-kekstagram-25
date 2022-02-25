b321e3344af06dae22fecbb2ce0b95417beb71c3
Jeenlevel <inlevel94@gmail.com>
2022-02-25 15:22:17 +0300

    исправления 2

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function randomInteger(min, max) {
   let rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 };
-console.log(randomInteger(1, 3));
+randomInteger(1,3);
 
 function stringLength(сheckinString,maxLength) {
   if (length <= maxLength) {
@@ -11,4 +11,4 @@ function stringLength(сheckinString,maxLength) {
   }
   return true;
 }
-  console.log(stringLength(1,9));
+stringLength(1,9);
