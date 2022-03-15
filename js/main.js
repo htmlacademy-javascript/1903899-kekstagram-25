4cfd86230f7ae2bc456f0f1ff76498852aae2783
Jeenlevel <inlevel94@gmail.com>
2022-03-15 13:06:17 +0300

    Получаем функции от Кекса

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-const getRandomInteger = (min = 0, max) => {
+/* const getRandomInteger = (min = 0, max) => {
   const rand = min + Math.random() * (max + 1 - min);
 
   return Math.floor(rand);
 };
-getRandomInteger(3);
+getRandomInteger(3); */
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+getRandomPositiveInteger(1,5);
 
-const checkStringLength = (string, maxLength) => string.length <= maxLength;
+const checkStringLength = (string, length) => string.length <= length;
 checkStringLength('hello', 4);
