873ca4a56bce9af1b58413f8096a6bad976b9eba
Jeenlevel <inlevel94@gmail.com>
2022-04-11 23:45:32 +0300

    module9

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 import './data.js';
-import './big-picture';
 import './pictures';
+import './user-form.js';
+import './api.js';
+import {getData} from './api.js';
+import {renderSimilarListPictures} from './pictures';
 
+getData((similarPictures) => {
+  renderSimilarListPictures(similarPictures);
+});
